6e188864763f15f70d85d707aae8c788d0dd000c
kashishgadhiya <kashishgadhiya24@gmail.com>
2024-07-21 23:16:58 +0530

    banner changes

--- src/app/Components/HomeBanners.js
+++ src/app/Components/HomeBanners.js
@@ -1,36 +1,32 @@
-import React from 'react'
-import Banner from './Banner'
-import { Box } from '@chakra-ui/react'
+import React from 'react';
+import Banner from './Banner';
 
 const HomeBanners = () => {
   return (
-    <Box>
-        <Banner
-      purpose='RENT A HOME'
-      title1='Rental Homes for'
-      title2='Everyone'
-      desc1=' Explore from Apartments, builder floors, villas'
-      desc2='and more'
-      buttonText='Explore Renting'
-      linkName='/RentList'
-      imgUrl='/b_home.jpg'
-    />
-<div className='flex flex-wrap'>
+    <div className='space-y-8'> 
+      <Banner
+        purpose='RENT A HOME'
+        title1='Rental Homes for'
+        title2='Everyone'
+        desc1='Explore from Apartments, builder floors, villas'
+        desc2='and more'
+        buttonText='Explore Renting'
+        linkName='/RentList'
+        imgUrl='/b_home.jpg'
+      />
+     
+      <Banner
+        purpose='BUY A HOME'
+        title1='Find, Buy & Own Your'
+        title2='Dream Home'
+        desc1='Explore from Apartments, land, builder floors,'
+        desc2='villas and more'
+        buttonText='Explore Buying'
+        linkName='/BuyList'
+        imgUrl='/b_buy.jpg'
+      />
+    </div>
+  );
+};
 
-</div>
-   <Banner
-      purpose='BUY A HOME'
-      title1=' Find, Buy & Own Your'
-      title2='Dream Home'
-      desc1=' Explore from Apartments, land, builder floors,'
-      desc2=' villas and more'
-      buttonText='Explore Buying'
-      linkName='/BuyList'
-      imgUrl='/b_buy.jpg'
-      
-    />
-    </Box>
-  )
-}
-
-export default HomeBanners
+export default HomeBanners;
